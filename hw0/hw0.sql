0238e39981daa31ff78cf304d96988f466e8214c
jessie <overmars34@hotmail.com>
2018-11-27 08:17:03 -0800

    Modify TODO list

--- hw0/hw0.sql
+++ hw0/hw0.sql
@@ -1,5 +1,7 @@
 /*This file is a test file. You will never add new files to the repository in your homework assignments, and always modify designated files.*/
 
-/*TODO: Fill Something Here */
+/*TODO: */
+/*Finish this course before Christmas*/
+/*Learn how to calculate complexity*/
 
 /*The rest of the file continues here*/
