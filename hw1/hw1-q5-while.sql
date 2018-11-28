44145253de552377338d0820770586981ea81179
jessie <overmars34@hotmail.com>
2018-11-28 15:41:11 -0800

    Finish q5

--- hw1/hw1-q5-while.sql
+++ hw1/hw1-q5-while.sql
@@ -3,17 +3,32 @@
 DROP TABLE IF EXISTS q5_extnded_paths;
 CREATE TABLE q5_extended_paths(src, dest, length, path)
 AS
-	SELECT -- remember to remove duplicates!
+select distinct u.src, e.dest,
+(select u1.length + 1 from q5_paths_to_update u1 where u1.src = u.src and u1.dest = u.dest),
+(select u2.path || e.dest from q5_paths_to_update u2 where u2.src = u.src and u2.dest = u.dest)
+from q5_edges e, q5_paths_to_update u
+where u.dest = e.src
+and u.dest <> e.dest
 ;
 
 CREATE TABLE q5_new_paths(src, dest, length, path)
 AS
-	SELECT -- fill this in
+select u.*
+from q5_paths_to_update u
+where (not exists (select 1 from q5_paths p where p.src = u.src and p.dest = u.dest)
+or exists (select 1 from q5_paths p where p.src = u.src and p.dest = u.dest and p.length > u.length))
 ;
 
 CREATE TABLE q5_better_paths(src, dest, length, path)
 AS
-	SELECT -- fill this is
+select p.src, p.dest, p.length, p.path
+from q5_paths p
+where (not exists (select 1 from q5_new_paths n where p.src = n.src and p.dest = n.dest)
+or exists (select 1 from q5_new_paths n where p.src = n.src and p.dest = n.dest and p.length <= n.length))
+union all
+select n.src, n.dest, n.length, n.path
+from q5_new_paths n
+where exists (select 1 from q5_paths p where p.src = n.src and p.dest = n.dest and p.length > n.length) 
 ;
 
 DROP TABLE q5_paths;
