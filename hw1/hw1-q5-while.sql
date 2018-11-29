8cca8e1c434475aac5ba28a4c2ff6ab87aeb391a
jessie <overmars34@hotmail.com>
2018-11-28 21:25:40 -0800

    Modify q5 to the correct path_count with one SELECT 332?

--- hw1/hw1-q5-while.sql
+++ hw1/hw1-q5-while.sql
@@ -1,38 +1,32 @@
 --Q5 Iteration
 
-DROP TABLE IF EXISTS q5_extnded_paths;
+DROP TABLE IF EXISTS q5_extended_paths;
 CREATE TABLE q5_extended_paths(src, dest, length, path)
 AS
-select distinct u.src, e.dest,
-(select u1.length + 1 from q5_paths_to_update u1 where u1.src = u.src and u1.dest = u.dest),
-(select u2.path || e.dest from q5_paths_to_update u2 where u2.src = u.src and u2.dest = u.dest)
+select distinct u.src, e.dest, u.length + 1, u.path || e.dest
 from q5_edges e, q5_paths_to_update u
 where u.dest = e.src
-and u.dest <> e.dest
+and u.src != e.dest
 ;
 
 CREATE TABLE q5_new_paths(src, dest, length, path)
 AS
-select u.*
-from q5_paths_to_update u
-where (not exists (select 1 from q5_paths p where p.src = u.src and p.dest = u.dest)
-or exists (select 1 from q5_paths p where p.src = u.src and p.dest = u.dest and p.length > u.length))
+select * from q5_extended_paths e
+where not exists (select 1 from q5_paths p where p.src = e.src and p.dest = e.dest)
+union
+select * from q5_extended_paths e
+where exists (select 1 from q5_paths p where p.src = e.src and p.dest = e.dest and p.length > e.length)
 ;
 
 CREATE TABLE q5_better_paths(src, dest, length, path)
 AS
-select p.src, p.dest, p.length, p.path
-from q5_paths p
-where (not exists (select 1 from q5_new_paths n where p.src = n.src and p.dest = n.dest)
-or exists (select 1 from q5_new_paths n where p.src = n.src and p.dest = n.dest and p.length <= n.length))
-union all
-select n.src, n.dest, n.length, n.path
-from q5_new_paths n
-where exists (select 1 from q5_paths p where p.src = n.src and p.dest = n.dest and p.length > n.length) 
+select * from q5_new_paths
+union
+select * from q5_paths 
 ;
 
 DROP TABLE q5_paths;
-ALTER TABLE q5_better_paths RENAME q5_paths;
+ALTER TABLE q5_better_paths RENAME TO q5_paths;
 
 DROP TABLE q5_paths_to_update;
 ALTER TABLE q5_new_paths RENAME TO q5_paths_to_update;
